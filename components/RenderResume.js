7ee94f7a68a42ab016b970c19283d7445c42e009
Blake Prall <127055195+bprall@users.noreply.github.com>
2024-07-09 12:48:34 -0500

    Update RenderResume.js

--- components/RenderResume.js
+++ components/RenderResume.js
@@ -1,8 +1,8 @@
 import renderNavbar from "./RenderNav.js";
 
 export default function renderResume(resume) {
-  const body = document.querySelector("body");
-  body.innerHTML = `
+  const main = document.querySelector("main");
+  main.innerHTML = `
     <section class="project">
       <iframe src="${resume}" 
               type="application/pdf" style="width: 100%; height: 100vh;" scrolling="yes"></iframe>
